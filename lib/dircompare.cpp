2e3e855db31b9ce2a47bc8adbd3bf0bf6866be9c
Kimmo Varis <kimmov@gmail.com>
2011-03-08 23:30:12 +0200

    Working a bit for the directory compare.
    Now gets a sorted list of QFileInfos and iterates those lists. Prints debug
    messages about skipped directories and one-side-only files.

--- lib/dircompare.cpp
+++ lib/dircompare.cpp
@@ -3,6 +3,7 @@
 #include <QString>
 #include <QList>
 #include <QStringList>
+#include <QtDebug>
 #include "dircompare.h"
 #include "filecompare.h"
 #include "resultitem.h"
@@ -21,29 +22,52 @@ DirCompare::~DirCompare()
 
 void DirCompare::DoCompare()
 {
-    QStringList left = mLeftDir->entryList();
-    QStringList right = mRightDir->entryList();
+    const QFileInfoList left = mLeftDir->entryInfoList(QDir::NoFilter,
+                               QDir::Name | QDir::DirsFirst);
+    const QFileInfoList right = mRightDir->entryInfoList(QDir::NoFilter,
+                                QDir::Name | QDir::DirsFirst);
 
     mResults.clear();
 
-    QStringList::const_iterator iter = left.constBegin();
-    while (iter != left.constEnd())
+    QFileInfoList::const_iterator iter = left.constBegin();
+    QFileInfoList::const_iterator iter2 = right.constBegin();
+    while (iter != left.constEnd() && iter2 != right.constEnd())
     {
-        int index = right.indexOf(*iter);
-        if (index != -1)
+        const QFileInfo inf1 = *iter;
+        const bool leftIsFile = inf1.isFile();
+        const QFileInfo inf2 = *iter2;
+        const bool rightIsFile = inf2.isFile();
+
+        if (leftIsFile && rightIsFile)
         {
-            QFileInfo inf1(*iter);
-            const bool leftIsFile = inf1.isFile();
-            QFileInfo inf2(right.at(index));
-            const bool rightIsFile = inf2.isFile();
-            if (leftIsFile && rightIsFile)
+            if (inf1.fileName() == inf2.fileName())
             {
-                FileCompare compare(*iter, right.at(index));
+                qDebug() << "Comparing: " << inf1.fileName() << " and " << inf2.fileName();
+                FileCompare compare(inf1.absoluteFilePath(), inf2.absoluteFilePath());
                 ResultItem item = compare.Compare();
                 mResults << item;
+                ++iter;
+                ++iter2;
+            }
+            else
+            {
+                qDebug() << "Skipping one-side item: " << inf1.fileName();
+                ++iter;
             }
         }
-        ++iter;
+        else
+        {
+            if (!leftIsFile)
+            {
+                qDebug() << "Skipping left-side dir: " << inf1.baseName();
+                ++iter;
+            }
+            if (!rightIsFile)
+            {
+                qDebug() << "Skipping right-side dir: " << inf2.baseName();
+                ++iter2;
+            }
+        }
     }
 }
 
